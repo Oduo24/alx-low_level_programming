0e4c7352927deedf7cd4b10ca27734a3c04936b3
Oduo24 <geraldoduo@gmail.com>
2022-03-11 17:51:36 +0000

    1-compiler

--- 0x00-hello_world/hello.c
+++ 0x00-hello_world/hello.c
@@ -1,10 +1,12 @@
+/*Program description*/
 #include <stdio.h>
 /**
  *First hello world program
  *Always return 0 (success)
  */
-int main(void)
+int main(void) /*function description*/
 {
 	printf("Hello World");
-	return (0);
+	return (0); /*Return description*/
 }
+
