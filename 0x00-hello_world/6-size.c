d00564912a8cbd43f0aa20ff20f50f00da83ae92
Gerald <geraldoduo@email.com>
2022-03-11 23:34:57 +0300

    added task 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,5 @@ printf("Size of an int: %lu\n", sizeof(int));
 printf("Size of a long int: %lu\n", sizeof(long int));
 printf("Size of a long long int: %lu\n", sizeof(long long int));
 printf("Size of a float: %lu\n", sizeof(float));
+return (0);
 }
