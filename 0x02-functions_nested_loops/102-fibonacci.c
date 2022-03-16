cd4cc54f9a005a61ebfaabe811ea08530aee7f01
Oduo24 <geraldoduo@gmail.com>
2022-03-16 23:52:15 +0000

    Solution for task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *main - calls the phi function*
+ *@x: integer
+ *@y: int one step behind x
+ *@i: counter
+ *Return: 0 if successful
+ */
+void phi(unsigned long x, unsigned long y, int i);
+int main(void)
+{
+	phi(2, 1, 1);
+	return (0);
+}
+/**
+ *phi - fibonacci sequence
+ *@x: integer
+ *@y: one step behind x
+ *@i: counter
+ */
+void phi(unsigned long x, unsigned long y, int i)
+{
+	i++;
+	if (i <= 50)
+	{
+		unsigned long z = x;
+
+		printf("%lu, ", y);
+		x = x + y;
+		y = z;
+		phi(x, y, i);
+	}
+	else
+	{
+		printf("%lu", y);
+		printf("\n");
+	}
+}
+
