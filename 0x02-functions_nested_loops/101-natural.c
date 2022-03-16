158133790c0a7fe176674081274f0a8329452947
Oduo24 <geraldoduo@gmail.com>
2022-03-16 23:46:17 +0000

    Solution for task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *main - function gives the sum of all multiples of 3 and 5 less than 1024 not included
+ *main - function gives the sum of multiples of 3 and 5 upto 1024
  *Return:  if successful
  */
 
 int main(void)
 {
 	int n, sum;
-	sum = 0;
 
+	sum = 0;
 	for (n = 1023; n > 0; n--)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
