a10bd4493649a9c27cc256600a217ecdfa724611
Oduo24 <geraldoduo@gmail.com>
2022-03-16 17:27:25 +0000

    added solution for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  *main - entry point
  *Return: - Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
+ *
+*int main(void)
+*{
+*	print_alphabet();
+*	return (0);
+*}
+*/
 void print_alphabet(void)
 {
 	int i;
