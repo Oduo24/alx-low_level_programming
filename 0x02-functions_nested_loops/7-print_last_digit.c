bcd33d42528c20f7f9af8c283530baaee6941fca
Oduo24 <geraldoduo@gmail.com>
2022-03-16 22:19:33 +0000

    Solution for task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_last_digit - Entry point
+ *@n: input
+ *Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	if ((n % 10) < 0)
+	{
+	_putchar((-1 * (n % 10)) + 48);
+	return (-1 * (n % 10));
+	}
+	else
+	{
+	_putchar((n % 10) + 48);
+	return (n % 10);
+	}
+}
+
