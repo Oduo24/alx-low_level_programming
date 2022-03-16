3c115b583e00562a31c1f2f4b91acab4ea527ebb
Oduo24 <geraldoduo@gmail.com>
2022-03-16 18:26:00 +0000

    Added solution to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ *print_alphabet - outputs alphabets a-z
+ *Return: - Always void (Success)
+ *
+ **/
+void print_alphabet(void)
+{
+int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(alphabet[i]);
+		}
+	}
+	_putchar('\n');
+}
