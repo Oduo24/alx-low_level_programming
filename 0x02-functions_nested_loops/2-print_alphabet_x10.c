e5822d1d108ec7fe9ac99e1fcb5a6be2233489ed
Oduo24 <geraldoduo@gmail.com>
2022-03-16 19:14:45 +0000

    Added solution to task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-int i,j;
+int i, j;
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 26; i++)
 		{
 			_putchar(alphabet[i]);
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+
 }
 
