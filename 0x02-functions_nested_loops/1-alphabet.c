bf8fb89ceb394dcc5e51129a5535a7d400253c21
Oduo24 <geraldoduo@gmail.com>
2022-03-16 16:57:50 +0000

    added solution for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *main - entry point
+ *Return: - Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+}
+
+void print_alphabet(void)
+{
+	int i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz"
+		for (i = 0; i < 27; i++)
+		{
+			_putchar(alphabet[i]);
+		}
+	_putchar('\n');
+	return (0);
+}
+
