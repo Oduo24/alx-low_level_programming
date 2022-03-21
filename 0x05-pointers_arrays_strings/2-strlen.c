cf5ffb9fd883a56d4ffaf27781cc19e7dc6f296b
Oduo24 <geraldoduo@gmail.com>
2022-03-21 21:31:04 +0000

    Added solution to task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
  * _strlen - determines the length of a string
- * Return: int i
+ * Return: int result
  * @s: pointer to string
  */
 int _strlen(char *s)
 {
 	int i;
+	int result;
 
 	for (i = 0; s[i] != '\0'; i++)
-	return (i);
+	{
+		result = i;
+	}
+	return (result);
 }
