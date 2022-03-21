a8fe527eab969bd3c2fc786f2170b42212e8606d
Oduo24 <geraldoduo@gmail.com>
2022-03-21 22:24:00 +0000

    Added solution to task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * Return: void
+ * @str: pointer to the string
+ */
+void _puts(char *str)
+{
+	int i;
+	strng = *str;
+
+	for (i = 0; strng[i] != '\0'; i++)
+	{
+		_putchar(strng[i]);
+	}
+	_putchar('\n');
+}
