31f901c85114089aa65af93cb973f8bce4c5e611
Oduo24 <geraldoduo@gmail.com>
2022-03-21 23:06:41 +0000

    Added solution to task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+/**
+ * rev_string - reverses a string
+ * Return: void
+ * @s: pointer to the string
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	while(s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
