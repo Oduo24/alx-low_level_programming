b76cb3c05075c463f9141ef68f06cdeef8e0d0f2
Oduo24 <geraldoduo@gmail.com>
2022-03-24 22:45:11 +0000

    Added solution to task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,14 +3,14 @@
  * _strncpy - copies from src to dest
  * @dest: destination
  * @src: source
- * @n: number of arrays
- * Return: (pointer to dest)
+ * @n: number of characters/bytes
+ * Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i <= n; i++)
 		dest[i] = src[i];
 
 	for (; i < n; i++)
