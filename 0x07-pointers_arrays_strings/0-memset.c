44f8d69d6860a01399cc8c4d8832f8edec9216b8
Oduo24 <geraldoduo@gmail.com>
2022-03-28 19:16:35 +0000

    used while loop instead of for loop

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		s[i] = (unsigned char)b;
-		return (s);
-	}}
+		s[i] = b;
+		i++;
+	
+	}	
+	return (s);
+}
