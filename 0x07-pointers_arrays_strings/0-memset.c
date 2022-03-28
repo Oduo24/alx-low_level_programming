caab7d9a5d4a4fbdee9705d7404a6ce5436b1caf
Oduo24 <geraldoduo@gmail.com>
2022-03-28 14:19:05 +0000

    added solution to project 0x07 Task 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ * *_memset - function that acts as the memset standard c function
+ * @s: memory location
+ * @b: byte to be filled
+ * @n: number of times the b byte should be pplacedin the memory
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		return (s);
+	}
+}
