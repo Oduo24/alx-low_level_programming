250bb78c7781dd2556fa08d62332b2d601ec1cf0
Oduo24 <geraldoduo@gmail.com>
2022-04-11 22:12:12 +0000

    struct and type def corrections

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free's the dog
+ * @d: dog to free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		d = NULL;
+	}
+}
