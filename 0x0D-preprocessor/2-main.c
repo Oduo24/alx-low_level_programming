6d202365015a788a1c9d4934f1e16fbd661d544b
Oduo24 <geraldoduo@gmail.com>
2022-04-11 22:08:36 +0000

    solutions to preprocessors, structures and typedef

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define FILENAME __FILE__
+/**
+ * main - prints filename before compilation
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf(FILENAME "\n");
+	return (0);
+}
